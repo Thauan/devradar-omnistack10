b438bf1f208c6ccf1eda0a2b83a97574c9646b27
Thauan <thaubr28@gmail.com>
2020-01-19 09:55:13 -0300

    Lesson 2

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://omnistack10:omnistack10@cluster0-pkcv4.mongodb.net/week10?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(routes);
+app.use(express.json());
+
+app.listen(3333);
